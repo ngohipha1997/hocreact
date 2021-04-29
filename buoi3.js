616c70b0a3112ed496ce84b072efcd892ea5c0dc
ngohipha1997 <ngohipha5@gmail.com>
2021-04-29 18:40:35 +0700

    accumulator/currentValue

--- buoi3.js
+++ buoi3.js
@@ -47,6 +47,46 @@
 // console.log(newArrNames)
 
 
+// const arrPersom = [
+//     {id : 1 , name : "Teo" , age : 20, },
+//     {id : 2, name : "Te0" , age : 25, },
+//     {id : 3 , name : "Teoo" , age : 22, },
+//     {id : 4 , name : "Tuan" , age : 21, },
+//     {id : 5 , name : "Lan" , age : 26, },
+// ]
+// // Yeu cau in ra mang person duoi 25t
+// const filterArrPerson = arrPersom.filter(function(person){
+//     if(person.age < 25){
+//         return true ;
+//     }
+//     return false;
+
+// }).map(function(object){
+//     return object.name
+// })
+// console.log(filterArrPerson)
+
+//phuong thuc find ??
+
+
+//16 ham reduce
+//  const arrNums =[1,2,3,4,5]
+
+//  const newArrNums = arrNums.reduce(function(accumulator,currentValuve){
+//      // lan 1 : accumulator : 0 bang gia tri duoi (value)
+//      // currentValue : 1 vao phan tu dau tien 
+//      //return ve 5 
+//      // lan 2:
+//      // accumuLartor : 5 
+//      // currentvalue : 2 
+//      //return 7
+//      // accumuLartor : 7 
+//      // currentvalue : 3 
+//         return accumulator +currentValuve
+//  },0)
+
+//  console.log(newArrNums)
+
 const arrPersom = [
     {id : 1 , name : "Teo" , age : 20, },
     {id : 2, name : "Te0" , age : 25, },
@@ -54,14 +94,10 @@ const arrPersom = [
     {id : 4 , name : "Tuan" , age : 21, },
     {id : 5 , name : "Lan" , age : 26, },
 ]
-// Yeu cau in ra mang person duoi 25t
-const filterArrPerson = arrPersom.filter(function(person){
-    if(person.age < 25){
-        return true ;
+conts newArr = arrPersom.reduce(function(accumulator ,currentValue){
+    if (currentValue.age < 25){
+        accumulator.push(currentValue.name);
     }
-    return false;
-
-}).map(function(object){
-    return object.name
-})
-console.log(filterArrPerson)
+    return accumulator
+},[])
+console.log(newArr)
